5ed40c8a7205ecdc720b1474483b8aa173807909
Jamie Moore <jamie@thompsonbrandpartners.com>
2020-09-08 22:28:14 +0100

    added a reset button to my counter

--- button-counter/clicker.js
+++ button-counter/clicker.js
@@ -7,3 +7,8 @@ document.querySelector('#clicker').onclick = function counterAdd() {
     count ++;
     counterText.textContent = `${count}`;
 }
+
+document.querySelector('#reset').onclick = function reset() {
+    count = 0;
+    counterText.textContent = `${count}`;
+}
